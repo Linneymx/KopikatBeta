6ac60cfd36d6df0b54c12ee5261a26374c24534c
unknown <linneymx>
2022-09-26 00:04:04 -0500

    correction img

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,8 +15,8 @@ function Navbar() {
     <div className="navbar">
       <div className="leftSide"
       id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
-        
+           <img src={Logo}  alt="50px"/>
+           
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/menu"> Menu </Link>
